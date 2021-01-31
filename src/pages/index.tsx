83e525df974d91df3a2d407636a1d9d7a754eeec
Bo Kamphues <b.kamphues@outlook.com>
2021-01-31 16:07:36 +0100

    Cleaned node_modules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from "react"
+import ProjectCard from "../components/project-card"
 
 export default function Index() {
   return (
     <div>
       Hello world! This is a test.<br></br>
-      <a href="./projects">Projects</a>
+      <ProjectCard></ProjectCard>
     </div>
   )
 }
