ae4ca371bac9c60735f70e34c825dafb40f1b7e8
Bo Kamphues <b.kamphues@outlook.com>
2021-02-12 14:54:57 +0100

    Working on project-card

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,18 +1,29 @@
 import React from "react"
 
+function checkPropType(prop, type) {
+  if (prop == null) {
+    throw new Error(`No data was given for ${type}`)
+  }
+}
+
 export default function ProjectCard(props) {
   // project card component
+
+  // check data
+  checkPropType(props.year, "year")
+  checkPropType(props.title, "title")
+  checkPropType(props.children, "children")
+
+  // return component
   return (
-    <div>
-      <h5 className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-        {props.year}
-      </h5>
-      <h4 className="text-lg leading-tight font-medium text-black">
-        {props.title}
-      </h4>
-      <p className="text-gray-500">{props.children}</p>
-      {props.gh_link && <a href={props.gh_link}>Github</a>}
-      {props.pr_link && <a href={props.pr_link}></a>}
+    <div className="p-2">
+      <div className="bg-white rounded shadow-lg max-w-sm">
+        <h5 className="font-sans text-xl px-2 pt-2 font-light">{props.year}</h5>
+        <h4 className="font-sans text-lg px-2 font-bold">{props.title}</h4>
+        <p className="px-2 pb-2 text-sm">{props.children}</p>
+        {props.gh_link && <a href={props.gh_link}>Github</a>}
+        {props.pr_link && <a href={props.pr_link}></a>}
+      </div>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,10 @@ import ProjectCard from "../components/project-card"
 
 export default function Index() {
   return (
-    <div>
+    <body className="bg-indigo-100">
       <ProjectCard title="Test" year="2021">
         This is a test project to showcase the cards.
       </ProjectCard>
-    </div>
+    </body>
   )
 }
