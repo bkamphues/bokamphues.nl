cf63b9d21540b026fd2facf1015cf80544ddb963
Bo Kamphues <b.kamphues@outlook.com>
2021-02-12 15:38:16 +0100

    Added colour pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: ["./src/**/*.{js, jsx, ts, tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: "#083D77",
+      secondary: "#F9C22E",
+      "primary-light": "#6CD4FF",
+      rose: "#F56476",
+      "rose-dark": "#E43F6F",
+    },
     fontFamily: { sans: ["Inter", "sans-serif"] },
     extend: {},
   },
