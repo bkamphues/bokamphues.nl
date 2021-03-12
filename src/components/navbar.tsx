30de3a9d404d883e9631c64322d4ff8e8d65327e
Bo Kamphues <b.kamphues@outlook.com>
2021-03-12 15:27:13 +0100

    Working on navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,54 +1,6 @@
 import React from "react";
-import { graphql, Link, useStaticQuery } from "gatsby";
-import { validURL } from "../util/siteLib";
-
-function NavigationLinks() {
-	// retrieve link data from site settings
-	const data = useStaticQuery(graphql`
-		query {
-			allSiteSettingsYaml {
-				edges {
-					node {
-						id
-						nav_links {
-							link_name
-							link_url
-						}
-					}
-				}
-			}
-		}
-	`);
-
-	// filter the data for navigation links
-	let links = data.allSiteSettingsYaml.edges.filter(
-		setting => setting.node.nav_links != null
-	)[0].node.nav_links;
-
-	// validate each URL and return html
-	let navigationLinks = [];
-	links.forEach(link => {
-		if (validURL(link.link_url)) {
-			navigationLinks.push(
-				<a href={link.link_url} target="_blank" rel="noopener noreferrer">
-					{link.link_name}
-				</a>
-			);
-		} else {
-			navigationLinks.push(
-				<Link
-					className="px-3 py-2 text-md rounded-md text-white hover:bg-gray-700 font-semibold"
-					to={link.link_url}
-				>
-					{link.link_name}
-				</Link>
-			);
-		}
-	});
-
-	// return link item
-	return <ul>{navigationLinks}</ul>;
-}
+import NavigationLinks from "./navbar-links";
+import NavigationLogos from "./navbar-logos";
 
 export default class Navbar extends React.Component {
 	// constructor hook
@@ -59,8 +11,18 @@ export default class Navbar extends React.Component {
 	// render hook
 	render() {
 		return (
-			<nav className="bg-gray-800 p-4 shadow-md">
-				<NavigationLinks />
+			<nav className="bg-gray-800 shadow-md">
+				<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+					<div className="relative flex items-center justify-between h-16">
+						<div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+							<NavigationLogos />
+							<NavigationLinks
+								activeClassName="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-bold"
+								className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-bold"
+							/>
+						</div>
+					</div>
+				</div>
 			</nav>
 		);
 	}
