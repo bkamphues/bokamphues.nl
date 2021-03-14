2f6a2b3a50decadf70e9524222363cbd999f6fb0
Bo Kamphues <b.kamphues@outlook.com>
2021-03-14 17:39:38 +0100

    Added ttile

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Hero() {
-	return <div className="w-full"></div>;
+	return (
+		<div className="w-full flex items-center">
+			<h1 className="max-w-sm text-6xl text-center">
+				Hey, my name is Bo Kamphues. Pipeline Technical Director
+			</h1>
+		</div>
+	);
 }
