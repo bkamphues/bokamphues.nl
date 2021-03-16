962edc04282e6015c8d4ef7688be2babc39339ca
Bo Kamphues <b.kamphues@outlook.com>
2021-03-16 16:32:55 +0100

    Changed fonts

--- src/components/technologies.tsx
+++ src/components/technologies.tsx
@@ -6,10 +6,10 @@ import { IconContext } from "react-icons/lib";
 export default function Technologies(): JSX.Element {
 	return (
 		<div className="pt-16 pb-5 container mx-auto">
-			<h1 className="mb-10 text-3xl text-center font-extrabold text-indigo-400">
+			<h1 className="mb-10 text-3xl text-center font-sans-alternate font-extrabold text-indigo-400">
 				Technologies
 			</h1>
-			<div className="max-w-xl mx-auto">
+			<div className="max-w-xl mx-auto mb-5">
 				<div className="grid grid-cols-3 gap-y-10 justify-items-center">
 					<IconContext.Provider value={{ color: "gray", size: "4em" }}>
 						{TechnologyIcons}
