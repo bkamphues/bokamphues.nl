4715505b37db8ee378f92badfc4c052f05b64d8a
Bo Kamphues <b.kamphues@outlook.com>
2021-03-16 14:19:36 +0100

    Added technology icons

--- src/components/technologies.tsx
+++ src/components/technologies.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Icons as TechnologyIcons } from "./technologies-icons";
+import { IconContext } from "react-icons/lib";
 
-export default function Technologies() {
+// react component to display all input technologies as a grayscale grid
+export default function Technologies(): JSX.Element {
 	return (
-		<div className="pt-16">
-			<h1>Technologies</h1>
+		<div className="pt-16 pb-5 container mx-auto">
+			<h1 className="mb-10 text-3xl text-center font-extrabold text-indigo-400">
+				Technologies
+			</h1>
+			<div className="max-w-xl mx-auto">
+				<div className="grid grid-cols-3 gap-y-10 justify-items-center">
+					<IconContext.Provider value={{ color: "gray", size: "4em" }}>
+						{TechnologyIcons}
+					</IconContext.Provider>
+				</div>
+			</div>
 		</div>
 	);
 }
