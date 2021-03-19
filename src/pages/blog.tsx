3962e9fed2a986c1f0ce56bbf5cb564a128a5276
Bo Kamphues <b.kamphues@outlook.com>
2021-03-19 12:50:56 +0100

    First version of blog mapping working

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -7,28 +7,50 @@ import { graphql } from "gatsby";
 import { getImage } from "gatsby-plugin-image";
 
 export const pageQuery = graphql`
-	query testImage {
-		allFile(filter: { extension: { eq: "jpg" } }) {
-			nodes {
-				childrenImageSharp {
-					gatsbyImageData(
-						placeholder: BLURRED
-						layout: FULL_WIDTH
-						formats: AUTO
-					)
-				}
+query blogArticles {
+	allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/(articles)/.*\\.md$/"}}) {
+	  edges {
+		node {
+		  frontmatter {
+			thumbnail {
+			  childImageSharp {
+				gatsbyImageData
+			  }
 			}
+			date
+			slug
+			tags
+			title
+		  }
+		  excerpt
 		}
+	  }
 	}
+  }  
 `;
 
 export default function Blog({ data }) {
+	const articles = data.allMarkdownRemark.edges;
+
 	return (
 		<Layout>
 			<SEO title="Blog" description="Overview page of all my blog articles." />
 			<div className="container mx-auto p-4 mb-5">
 				<h1 className="py-4 text-4xl font-bold text-indigo-400">Articles</h1>
-				<BlogGrid></BlogGrid>
+				<BlogGrid>
+					{articles.map(article => (
+						<BlogCard
+							title={article.node.frontmatter.title}
+							date={new Date(article.node.frontmatter.date)}
+							link={article.node.frontmatter.slug}
+							image={getImage(article.node.frontmatter.thumbnail)}
+							imageAlt="Test"
+							tags={article.node.frontmatter.tags}
+						>
+							{article.node.excerpt}
+						</BlogCard>
+					))}
+				</BlogGrid>
 			</div>
 		</Layout>
 	);
