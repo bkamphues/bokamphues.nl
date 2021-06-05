af0e0963b97a7f5f87a8da5b0edf9b5e7e799ca7
Bo Kamphues <b.kamphues@pm.me>
2021-06-05 18:47:33 +0200

    Changed plugin configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,66 +9,12 @@ const path = require("path");
 module.exports = {
 	/* Your site config here */
 	plugins: [
-		`gatsby-plugin-netlify-cms`,
 		`gatsby-plugin-postcss`,
-		`gatsby-transformer-yaml`,
 		`gatsby-plugin-react-helmet`,
 		`gatsby-plugin-sitemap`,
 		`gatsby-plugin-image`,
 		`gatsby-plugin-sharp`,
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `assets`,
-				path: path.join(__dirname, `static`, `assets`),
-			},
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `settings`,
-				path: path.join(__dirname, `content`, `site_settings`),
-			},
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `articles`,
-				path: path.join(__dirname, `content`, `articles`),
-			},
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `projects`,
-				path: path.join(__dirname, `content`, `projects`),
-			},
-		},
+		`gatsby-source-filesystem`,
 		`gatsby-transformer-sharp`,
-		{
-			resolve: `gatsby-transformer-remark`,
-			options: {
-				plugins: [
-					{
-						resolve: `gatsby-remark-relative-images`,
-					},
-					{
-						resolve: `gatsby-remark-images`,
-						options: { maxWidth: 1000 },
-					},
-					{
-						resolve: `gatsby-remark-prismjs`,
-						options: {},
-					},
-				],
-			},
-		},
 	],
-	siteMetadata: {
-		title: "Bo Kamphues",
-		titleTemplate: "%s · Bo Kamphues",
-		description:
-			"Personal portfolio, contact and blog website for Bo Kamphues.",
-		siteUrl: "https://www.bokamphues.nl",
-	},
 };
